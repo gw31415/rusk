3478ba23032d8794c84de18eb77a812d194de653
gw31415 <git@amas.dev>
2023-09-24 03:24:23 +0900

    Change in root directory detection method

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
-use std::{io, path::PathBuf};
+use std::{io, path::PathBuf, os::unix::prelude::OsStrExt};
 
 use deno_runtime::{deno_core::error::AnyError, tokio_util};
 use rusk::compose::Composer;
 
+const ROOT_PATTERNS: &[&[u8]] = &[
+    b".git",
+    b".svn",
+    b".hg",
+    b".bzr",
+    // ".gitignore",
+    b"Rakefile",
+    b"pom.xml",
+    b"project.clj",
+    b"package.json",
+    b"manifest.json",
+    // "*.csproj",
+    // "*.sln",
+];
+
 fn get_root() -> io::Result<PathBuf> {
-    use rusk::compose::RUSK_FILE;
     use std::{env::current_dir, fs::read_dir};
     let path = current_dir()?;
     let path_ancestors = path.as_path().ancestors();
     for p in path_ancestors {
-        let contains_ruskfile = read_dir(p)?.any(|p| p.unwrap().file_name() == *RUSK_FILE);
+        let contains_ruskfile = read_dir(p)?.any(|p| {
+            ROOT_PATTERNS.contains(
+                &{
+                    let Ok(entry) = p else { return false; };
+                    entry
+                }
+                .file_name()
+                .as_os_str()
+                .as_bytes(),
+            )
+        });
         if contains_ruskfile {
             return Ok(PathBuf::from(p));
         }

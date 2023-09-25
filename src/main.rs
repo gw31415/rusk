259a2758a74b9e11ca51d2ed33d76c26d346b67f
gw31415 <git@amas.dev>
2023-09-26 04:19:43 +0900

    comment and add helper options

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{io, os::unix::prelude::OsStrExt, path::PathBuf, process::exit};
+use std::{
+    io::{self, BufWriter, Write},
+    os::unix::prelude::OsStrExt,
+    path::PathBuf,
+    process::exit,
+};
 
 use clap::Parser;
 use deno::re_exports::deno_runtime::tokio_util;
+use env_logger::Env;
 use log::error;
 use rusk::compose::Composer;
 
@@ -21,9 +27,17 @@ const ROOT_PATTERNS: &[&[u8]] = &[
 ];
 
 #[derive(Parser)]
+/// Parallel processing, modern and fast Task Runner, written in Rust.
+#[command(author, version)]
 struct Args {
     /// Names of tasks to be executed
     taskname: Vec<String>,
+    /// List task names
+    #[arg(short, long)]
+    list: bool,
+    /// Log output in detail
+    #[arg(short, long)]
+    verbose: bool,
 }
 
 fn get_root() -> io::Result<PathBuf> {
@@ -49,11 +63,31 @@ fn get_root() -> io::Result<PathBuf> {
 }
 
 fn main() {
-    env_logger::init();
-    let Args { taskname } = Args::parse();
+    let Args {
+        taskname,
+        list,
+        verbose,
+    } = Args::parse();
+
+    // Setup env_logger
+    if verbose {
+        env_logger::init_from_env(Env::new().default_filter_or("info"));
+    } else {
+        env_logger::init();
+    }
 
     if let Err(err) = tokio_util::create_basic_runtime().block_on(async {
         let composer = Composer::new(get_root()?).await;
+
+        // List task names
+        if list {
+            let mut writer = BufWriter::new(io::stdout());
+            for name in composer.task_names() {
+                writeln!(writer, "{name}")?;
+            }
+        }
+
+        // Execute tasks
         composer.execute(taskname).await
     }) {
         error!("{err}");

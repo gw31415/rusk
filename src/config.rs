f399e055af8ead04ea366ba2c434ed39559acc9c
gw31415 <gw31415@amas.dev>
2025-01-09 16:03:16 +0900

    feat: separate files

---
+++ src/config.rs
@@ -0,0 +1,212 @@
+use std::{collections::HashMap, path::PathBuf};
+
+use deno_task_shell::{parser::SequentialList, ShellPipeReader, ShellPipeWriter, ShellState};
+use futures::future::try_join_all;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+
+/// Rusk error
+#[derive(Debug, thiserror::Error)]
+pub enum RuskError {
+    /// Task execution error
+    #[error(transparent)]
+    TaskError(#[from] TaskError),
+    /// Configuration parsing error
+    #[error(transparent)]
+    ConfigParseError(#[from] ConfigParseError),
+}
+
+/// Rusk configuration
+pub struct Rusk {
+    /// Tasks to be executed
+    pub tasks: HashMap<String, Task>,
+    /// Environment variables that are shared among all tasks
+    pub envs: HashMap<String, String>,
+}
+
+#[derive(Clone)]
+pub struct IOSet {
+    pub stdin: ShellPipeReader,
+    pub stdout: ShellPipeWriter,
+    pub stderr: ShellPipeWriter,
+}
+
+impl Default for IOSet {
+    fn default() -> Self {
+        Self {
+            stdin: ShellPipeReader::stdin(),
+            stdout: ShellPipeWriter::stdout(),
+            stderr: ShellPipeWriter::stderr(),
+        }
+    }
+}
+
+impl Rusk {
+    /// Execute tasks
+    pub async fn execute(self, io: IOSet) -> Result<(), RuskError> {
+        let parsed = ParsedConfig::try_from(self)?;
+        let fs = parsed
+            .tasks
+            .into_iter()
+            .map(|task| task.execute(io.clone()));
+        try_join_all(fs).await?;
+        Ok(())
+    }
+}
+
+/// Task configuration
+pub struct Task {
+    /// Environment variables that are specific to this task
+    pub envs: HashMap<String, String>,
+    /// Script to be executed
+    pub script: String,
+    /// Working directory
+    pub cwd: PathBuf,
+    /// Dependencies
+    pub depends: Vec<String>,
+}
+
+/// Configuration parsing error
+#[derive(Debug, thiserror::Error)]
+pub enum ConfigParseError {
+    /// Task is not executable because of dependency-related issues
+    #[error("Task {task_name:?} is not executable because of dependency-related issues")]
+    UnexecutableTask { task_name: String },
+    /// Task is defined multiple times
+    #[error("Task {task_name:?} is defined multiple times")]
+    DuplicateTaskName { task_name: String },
+    /// Task script parse error
+    #[error("Task {task_name:?} script parse error: {error:?}")]
+    ScriptParseError {
+        task_name: String,
+        error: anyhow::Error,
+    },
+}
+
+impl TryFrom<Rusk> for ParsedConfig {
+    type Error = ConfigParseError;
+
+    fn try_from(rusk: Rusk) -> Result<Self, Self::Error> {
+        let Rusk {
+            tasks,
+            envs: global_env,
+        } = rusk;
+        let mut tasks = tasks.into_iter().collect::<Vec<_>>();
+        tasks.sort_by(|(_, t1), (_, t2)| t1.depends.len().cmp(&t2.depends.len()));
+
+        let mut parsed_tasks: HashMap<String, ParsedTask> = HashMap::new();
+
+        for (task_name, task) in tasks {
+            if parsed_tasks.contains_key(&task_name) {
+                return Err(ConfigParseError::DuplicateTaskName { task_name });
+            }
+
+            let script = {
+                let mut items = Vec::new();
+                for line in task.script.lines() {
+                    items.extend(match deno_task_shell::parser::parse(line) {
+                        Ok(script) => script.items,
+                        Err(error) => {
+                            return Err(ConfigParseError::ScriptParseError { task_name, error })
+                        }
+                    });
+                }
+                SequentialList { items }
+            };
+
+            let mut depends = Vec::new();
+            for dep_name in &task.depends {
+                let (tx, rx) = channel::<Result<(), ()>>(1);
+                depends.push(rx);
+                if let Some(dep_task) = parsed_tasks.get_mut(dep_name) {
+                    dep_task.nexts.push(tx);
+                } else {
+                    return Err(ConfigParseError::UnexecutableTask { task_name });
+                }
+            }
+
+            let Task { envs, cwd, .. } = task;
+
+            parsed_tasks.insert(
+                task_name.clone(),
+                ParsedTask {
+                    task_name,
+                    script,
+                    envs: global_env.clone().into_iter().chain(envs).collect(),
+                    cwd,
+                    depends,
+                    nexts: Vec::new(),
+                },
+            );
+        }
+
+        Ok(ParsedConfig {
+            tasks: parsed_tasks.into_values().collect(),
+        })
+    }
+}
+
+pub type TaskResult = Result<(), TaskError>;
+
+#[derive(Clone, Debug, thiserror::Error)]
+#[error("Task {task_name:?} failed with exit code {exit_code}")]
+pub struct TaskError {
+    pub task_name: String,
+    pub exit_code: i32,
+}
+
+struct ParsedConfig {
+    tasks: Vec<ParsedTask>,
+}
+
+struct ParsedTask {
+    task_name: String,
+    envs: HashMap<String, String>,
+    script: SequentialList,
+    cwd: PathBuf,
+
+    depends: Vec<Receiver<Result<(), ()>>>,
+    nexts: Vec<Sender<Result<(), ()>>>,
+}
+
+impl ParsedTask {
+    async fn execute(self, io: IOSet) -> TaskResult {
+        let ParsedTask {
+            task_name,
+            envs,
+            script,
+            cwd,
+            depends,
+            nexts,
+        } = self;
+        if try_join_all(
+            depends
+                .into_iter()
+                .map(|mut rx| async move { rx.recv().await.unwrap() }),
+        )
+        .await
+        .is_err()
+        {
+            return Ok(());
+        }
+
+        let exit_code = deno_task_shell::execute_with_pipes(
+            script,
+            ShellState::new(envs, &cwd, Default::default(), Default::default()),
+            io.stdin,
+            io.stdout,
+            io.stderr,
+        )
+        .await;
+        if exit_code == 0 {
+            for tx in nexts {
+                tx.send(Ok(())).await.unwrap();
+            }
+            Ok(())
+        } else {
+            Err(TaskError {
+                task_name,
+                exit_code,
+            })
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,191 +1,14 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::collections::HashMap;
 
-use deno_task_shell::{parser::SequentialList, ShellPipeReader, ShellPipeWriter, ShellState};
-use futures::future::try_join_all;
-use tokio::sync::mpsc::{channel, Receiver, Sender};
+use config::{Rusk, RuskError, Task};
 
-#[derive(Debug, thiserror::Error)]
-pub enum RuskError {
-    #[error(transparent)]
-    TaskError(#[from] TaskError),
-    #[error(transparent)]
-    ConfigParseError(#[from] ConfigParseError),
-}
-
-pub struct Config {
-    pub tasks: HashMap<String, Task>,
-    pub envs: HashMap<String, String>,
-}
-
-impl Config {
-    pub async fn execute(
-        self,
-        stdin: ShellPipeReader,
-        stdout: ShellPipeWriter,
-        stderr: ShellPipeWriter,
-    ) -> Result<(), RuskError> {
-        let parsed = ParsedConfig::try_from(self)?;
-        let fs = parsed
-            .tasks
-            .into_iter()
-            .map(|task| task.execute(stdin.clone(), stdout.clone(), stderr.clone()));
-        try_join_all(fs).await?;
-        Ok(())
-    }
-}
-
-pub struct Task {
-    pub envs: HashMap<String, String>,
-    pub script: String,
-    pub cwd: PathBuf,
-    pub depends: Vec<String>,
-}
-
-#[derive(Debug, thiserror::Error)]
-pub enum ConfigParseError {
-    #[error("Task {task_name:?} is not executable because of dependency-related issues")]
-    UnexecutableTask { task_name: String },
-    #[error("Task {task_name:?} is defined multiple times")]
-    DuplicateTaskName { task_name: String },
-    #[error("Task {task_name:?} script parse error: {error:?}")]
-    ScriptParseError {
-        task_name: String,
-        error: anyhow::Error,
-    },
-}
-
-impl TryFrom<Config> for ParsedConfig {
-    type Error = ConfigParseError;
-
-    fn try_from(config: Config) -> Result<Self, Self::Error> {
-        let Config {
-            tasks,
-            envs: global_config,
-        } = config;
-        let mut tasks = tasks.into_iter().collect::<Vec<_>>();
-        tasks.sort_by(|(_, t1), (_, t2)| t1.depends.len().cmp(&t2.depends.len()));
-
-        let mut parsed_tasks: HashMap<String, ParsedTask> = HashMap::new();
-
-        for (task_name, task) in tasks {
-            if parsed_tasks.contains_key(&task_name) {
-                return Err(ConfigParseError::DuplicateTaskName { task_name });
-            }
-
-            let script = match deno_task_shell::parser::parse(&task.script) {
-                Ok(script) => script,
-                Err(error) => return Err(ConfigParseError::ScriptParseError { task_name, error }),
-            };
-
-            let mut depends = Vec::new();
-            for dep_name in &task.depends {
-                let (tx, rx) = channel::<Result<(), ()>>(1);
-                depends.push(rx);
-                if let Some(dep_task) = parsed_tasks.get_mut(dep_name) {
-                    dep_task.nexts.push(tx);
-                } else {
-                    return Err(ConfigParseError::UnexecutableTask { task_name });
-                }
-            }
-
-            let Task { envs, cwd, .. } = task;
-
-            parsed_tasks.insert(
-                task_name.clone(),
-                ParsedTask {
-                    task_name,
-                    script,
-                    envs: global_config.clone().into_iter().chain(envs).collect(),
-                    cwd,
-                    depends,
-                    nexts: Vec::new(),
-                },
-            );
-        }
-
-        Ok(ParsedConfig {
-            tasks: parsed_tasks.into_values().collect(),
-        })
-    }
-}
-
-pub type TaskResult = Result<(), TaskError>;
-
-#[derive(Clone, Debug, thiserror::Error)]
-#[error("Task {task_name:?} failed with exit code {exit_code}")]
-pub struct TaskError {
-    pub task_name: String,
-    pub exit_code: i32,
-}
-
-struct ParsedConfig {
-    tasks: Vec<ParsedTask>,
-}
-
-struct ParsedTask {
-    task_name: String,
-    envs: HashMap<String, String>,
-    script: SequentialList,
-    cwd: PathBuf,
-
-    depends: Vec<Receiver<Result<(), ()>>>,
-    nexts: Vec<Sender<Result<(), ()>>>,
-}
-
-impl ParsedTask {
-    async fn execute(
-        self,
-        stdin: ShellPipeReader,
-        stdout: ShellPipeWriter,
-        stderr: ShellPipeWriter,
-    ) -> TaskResult {
-        let ParsedTask {
-            task_name,
-            envs,
-            script,
-            cwd,
-            depends,
-            nexts,
-        } = self;
-        if try_join_all(
-            depends
-                .into_iter()
-                .map(|mut rx| async move { rx.recv().await.unwrap() }),
-        )
-        .await
-        .is_err()
-        {
-            return Ok(());
-        }
-
-        let exit_code = deno_task_shell::execute_with_pipes(
-            script,
-            ShellState::new(envs, &cwd, Default::default(), Default::default()),
-            stdin,
-            stdout,
-            stderr,
-        )
-        .await;
-        if exit_code == 0 {
-            for tx in nexts {
-                tx.send(Ok(())).await.unwrap();
-            }
-            Ok(())
-        } else {
-            Err(TaskError {
-                task_name,
-                exit_code,
-            })
-        }
-    }
-}
+mod config;
 
 #[tokio::main]
 async fn main() {
-    let envs: HashMap<_, _> = std::env::vars().collect();
     let cwd = std::env::current_dir().unwrap();
-    let config = Config {
-        envs: envs.clone(),
+    let config = Rusk {
+        envs: std::env::vars().collect(),
         tasks: [
             (
                 "task1".into(),
@@ -208,14 +31,7 @@ async fn main() {
         ]
         .into(),
     };
-    match config
-        .execute(
-            ShellPipeReader::stdin(),
-            ShellPipeWriter::stdout(),
-            ShellPipeWriter::stderr(),
-        )
-        .await
-    {
+    match config.execute(Default::default()).await {
         Ok(()) => println!("All tasks are done"),
         Err(e) => match e {
             RuskError::TaskError(e) => {

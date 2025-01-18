b9de89e73728bb63048c10a37b777aa097aadd8c
gw31415 <gw31415@amas.dev>
2025-01-19 01:06:06 +0900

    style: change format of TasksListItem

--- src/ruskfile.rs
+++ src/ruskfile.rs
@@ -43,11 +43,11 @@ impl Display for TasksListItem<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}\t", self.name)?;
         if let Some(description) = &self.description {
-            write!(f, "{} ", description.bold())?;
+            write!(f, "{}\t", description.bold())?;
         }
         write!(
             f,
-            "({} {})",
+            "{} {}",
             "in".dimmed().italic(),
             self.path.to_string_lossy().yellow().dimmed().italic(),
         )
